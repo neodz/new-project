b1c3ebbc3615887dc3ade3241560ad6920847b7d
neodz <aminegangster@gmail.com>
2021-06-09 21:36:58 +0100

    correct db

--- laravel/database/migrations/2021_06_08_224950_create_articles_table.php
+++ laravel/database/migrations/2021_06_08_224950_create_articles_table.php
@@ -19,20 +19,21 @@ class CreateArticlesTable extends Migration
 
             $table->enum('etat_article',['pending','accepted','rejected']);
             $table->string('designation');
-            $table->float('prix')->default(10);
+            $table->float('prix')->default(0);
             $table->integer('quantite')->default(1);
             $table->enum('type_transaction',['exchange','location','achter']);
             $table->string('adresse')->nullable();
             $table->string('marque')->nullable();
             $table->enum('category',['cat1','cat2','cat3','cat4'])->default('cat1');
-            $table->enum('methode_paiement',['Visa card','Master card','edahabia'])->nullable();
+            $table->enum('methode_paiement',['Visa card','Master card','Edahabia'])->nullable();
             $table->string('photo')->default('');
             $table->string('description')->default('');
             $table->float('tarif_livraison')->default(0);
-            // utilisateur of the article owner 
+
+            // utilisateur of the article owner
             $table->unsignedBigInteger('utilisateur_id');
             $table->foreign('utilisateur_id')->references('id')->on('utilisateurs')->onDelete('cascade');
-   
+
             $table->timestamps();
         });
     }
