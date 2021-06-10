21d1b6846d8530b4323268bbc5249925d4c3a40b
neodz <aminegangster@gmail.com>
2021-06-10 23:59:44 +0100

    relationships-models-all

--- laravel/app/Models/Article.php
+++ laravel/app/Models/Article.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    public function utilisateur()
+    {
+        return $this->belongsTo(Utilisateur::class);
+    }
+
+    public function locations()
+    {
+        return $this->hasMany(Location::class);
+    }
+    public function exchanges()
+    {
+        return $this->hasMany(Exchange::class);
+    }
+    public function transactionAchats()
+    {
+        return $this->hasMany(TransactionAchat::class);
+    }
 }
--- laravel/app/Models/Utilisateur.php
+++ laravel/app/Models/Utilisateur.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Utilisateur extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    public function locations()
+    {
+        return $this->hasMany(Location::class);
+    }
+    public function exchanges()
+    {
+        return $this->hasMany(Exchange::class);
+    }
+    public function transactionAchats()
+    {
+        return $this->hasMany(TransactionAchat::class);
+    }
 }
